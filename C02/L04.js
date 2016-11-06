efb69ed8fb81147670ba02427d7e1aa133a81c58
Hernan Y.Ke <yuriqiao@gmail.com>
2016-11-06 17:06:03 -0500

    L2-4 done

--- C02/L04.js
+++ C02/L04.js
@@ -11,10 +11,21 @@ var lineChart = new Chart(ctx, {
         borderColor: "#900",
         fill: false,
         data: [6,10,9,6,14,12,16,13,9,7,6,10]
+      },
+      {
+        label: "2016",
+        backgroundColor: "white",
+        borderWidth: 1,
+        borderColor: "#090",
+        fill: false,
+        data: [10,8,6,5,12,8,16,17,6,7,6,10]
       }
     ]
   },
   options: {
-    maintainAspectRatio: false
+     maintainAspectRatio: false,
+    scales: {
+      yAxes: [{stacked: true}]  //means adding previous data set
+    }
   }
 });
