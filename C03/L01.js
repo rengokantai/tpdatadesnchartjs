06bffe5d352ca0721f1d2bc06044bcce2efda6f5
Hernan Y.Ke <yuriqiao@gmail.com>
2016-11-06 17:17:48 -0500

    L3-1 start

---
+++ C03/L01.js
@@ -0,0 +1,19 @@
+var ctx = $("#Buffer.compare(buf1, buf2)-chart");
+var lineChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: [
+      {
+        label: "2015",
+        data: [10,8,6,5,12,8,16,17,6,7,6,10],
+        backgroundColor: "rgba(200,0,0,.5)",
+        borderColor: "rgb(200,0,0)",
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    maintainAspectRatio: false
+  }
+});
