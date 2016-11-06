94aaf89e5c1945cfea5efb9a678e5955907e6475
Hernan Y.Ke <yuriqiao@gmail.com>
2016-11-06 09:29:35 -0500

    init cmt

---
+++ C02/L02.js
@@ -0,0 +1,20 @@
+var ctx = $("#line-chart");
+var lineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: [
+      {
+        label: "2015",
+        data: [10,8,6,5,12,8,16,17,6,7,6,10],
+        backgroundColor:"rgba(200,0,0,.5)",
+        borderColor:"black",
+        borderWidth:2
+
+      }
+    ]
+  },
+  options:{
+    maintainAspectRatio:false
+  }
+});
