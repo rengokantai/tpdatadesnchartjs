a0477e10d4ea7e583190516c0482c2da86e28875
Hernan Y.Ke <yuriqiao@gmail.com>
2016-11-06 15:00:24 -0500

    dup

--- C02/L02.js
+++ C02/L02.js
@@ -18,3 +18,24 @@ var lineChart = new Chart(ctx, {
     maintainAspectRatio:false
   }
 });
+
+
+var ctx2 = $("#line-chart2");
+var lineChart2 = new Chart(ctx2, {
+  type: 'line',
+  data: {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: [
+      {
+        label: "2015",
+        data: [10,8,6,5,12,8,16,17,6,7,6,10],
+        borderColor:"black",
+        borderWidth:2
+
+      }
+    ]
+  },
+  options:{
+    maintainAspectRatio:false
+  }
+});
