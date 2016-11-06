fa3407f9a7ee68c307698503a9b0ad24b3fddf47
Hernan Y.Ke <yuriqiao@gmail.com>
2016-11-06 18:47:30 -0500

    L4-1 start

---
+++ C04/L01.js
@@ -0,0 +1,13 @@
+var ctx = $("#pie-chart");
+
+var pieLabels = ["Apple", "Pecan", "Key Lime", "Cherry", "Coconut"];
+
+var pieData = [10,10,8,4,1];
+
+var pieColors = [
+  "rgb(153,21,0)",
+  "rgb(189,90,45)",
+  "rgb(208,255,113)",
+  "rgb(255,36,0)",
+  "rgb(194,250,255)"
+];
